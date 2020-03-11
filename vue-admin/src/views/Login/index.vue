049c363a9f08e7cc1949e83e61a077b833b466c7
xiao <2401345934@qq.com>
2020-03-11 22:15:31 +0800

    项目起步

---
+++ vue-admin/src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="login">
+        <h1>我是</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "index"
+  };
+</script>
+
+<style scoped>
+    #login {
+        background: red;
+        height: 100vh;
+    }
+</style>
